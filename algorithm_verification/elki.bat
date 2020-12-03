8803d47735bcdd3f9fb4be3afc9a370b0a5f1e75
Michael Watholowitsch <michael.studium@gmail.com>
2020-12-03 21:29:15 +0100

    simple comparison elki vs own algorithm

--- algorithm_verification/elki.bat
+++
@@ -1,9 +0,0 @@
-java -jar elki-bundle-0.7.5.jar KDDCLIApplication ^
--dbc.in datasets\multiple-gaussian-2d_unlabeled.csv ^
--algorithm clustering.subspace.PreDeCon ^
--dbscan.epsilon 1 ^
--dbscan.minpts 8 ^
--predecon.delta 0.5 ^
--predecon.kappa 100 ^
--predecon.lambda 2 ^
--resulthandler AutomaticVisualization
